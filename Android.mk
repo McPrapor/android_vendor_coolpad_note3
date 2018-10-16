73730b2b296d9f81ff3be8b16ea8c3ac76a24745
McPrapor <max.terleckiy@gmail.com>
2018-10-16 15:22:56 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -21,4 +21,13 @@ LOCAL_SRC_FILES_64 := vendor/lib64/libdpframework.so
 LOCAL_MULTILIB := both
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libnvram
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_SRC_FILES_32 := vendor/lib/libnvram.so
+LOCAL_SRC_FILES_64 := vendor/lib64/libnvram.so
+LOCAL_MULTILIB := both
+include $(BUILD_PREBUILT)
+
 endif
