22763dc317346f6cebfff6662ee4d79203cbc5eb
McPrapor <max.terleckiy@gmail.com>
2019-02-14 12:37:06 +0100

    Update htc_v36bml_dugl-firmware-blobs.mk

--- htc_v36bml_dugl-firmware-blobs.mk
+++ htc_v36bml_dugl-firmware-blobs.mk
@@ -1,1 +1,1 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/htc_v36bml_dugl/htc_v36bml_dugl_firmware/firmware,system/vendor/firmware)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/htc_v36bml_dugl/firmware,system/vendor/firmware)
